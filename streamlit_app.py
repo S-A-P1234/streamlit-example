f13e59335e8d0b24d94bd4ff48a147737cdfb042
S-A-P1234 <120572271+S-A-P1234@users.noreply.github.com>
2023-04-19 23:06:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,10 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
+    title = st.text_input('Movie title', 'Life of Brian')
+    total_points = st.slider("First Number", 1, 5000, 2000)
+    num_turns = st.slider("Second Number", 1, 100, 9)
+    num_turns = st.slider("Third Number", 1, 5000, 1000)
     Point = namedtuple('Point', 'x y')
     data = []
 
