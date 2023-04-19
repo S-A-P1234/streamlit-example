cd98366d02c173a6f10e3fad21ba75810c3a4843
S-A-P1234 <120572271+S-A-P1234@users.noreply.github.com>
2023-04-19 23:17:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ import streamlit as st
 
 
 
-with st.echo(code_location='above'):
-    n1 = st.text_input('First Number', '1')
-    n2 = st.text_input('First Number', '2')
-    n3 = st.text_input('First Number','3')
-    num1 = int(n1)
-    num2 = int(n2)
-    num3 = int(n3)
-    l = [num1,num2,num3]
-    maxn = max(l)
-    
-    st.write('Max Number is: ', maxn)
+
+n1 = st.text_input('First Number', '1')
+n2 = st.text_input('First Number', '2')
+n3 = st.text_input('First Number','3')
+num1 = int(n1)
+num2 = int(n2)
+num3 = int(n3)
+l = [num1,num2,num3]
+maxn = max(l)
+
+st.write('Max Number is: ', maxn)
   
